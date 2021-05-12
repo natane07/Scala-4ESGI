331d3e9ce4aedcb0b54b0fd390a8984766782137
Natane <natane-b@hotmail.fr>
2021-05-12 20:58:46 +0200

    Init model Video with API

--- src/main/scala/model/youtube/Video.scala
+++ src/main/scala/model/youtube/Video.scala
@@ -2,10 +2,11 @@ package model.youtube
 
 import httpApi.HttpCallApi
 import utils.Parse
-
 import java.util.Date
 
-class Video {
+import model.youtube
+
+class Video() {
 
   var id:String = null
 
@@ -21,6 +22,8 @@ class Video {
 
   var defaultAudioLanguage:String = null
 
+  override def toString = s"Video($id, $publishedAt, $channelId, $title, $description, $tags, $defaultAudioLanguage)"
+
   def getRelatedVideos(): List[Video] ={
     var relatedVideos: List[Video] = List()
     val http = new HttpCallApi()
@@ -34,4 +37,28 @@ class Video {
     })
     relatedVideos
   }
+
+}
+
+object Video {
+
+  def getVideo (videoId: String): Video = {
+    // Call api
+    val http = new HttpCallApi()
+    val jsonResponse: String = http.youtubeVideos(videoId)
+    // Parse du json
+    val parseJson = new Parse()
+    val listYoutubeJson = parseJson.json(jsonResponse)
+    // Instance de video
+    val video = new Video()
+    video.id = listYoutubeJson.head.id
+    video.publishedAt = listYoutubeJson.head.publishedAt
+    video.channelId = listYoutubeJson.head.channelId
+    video.title = listYoutubeJson.head.title
+    video.description = listYoutubeJson.head.description
+    video.tags = listYoutubeJson.head.tags
+    video.defaultAudioLanguage = listYoutubeJson.head.defaultAudioLanguage
+    video
+  }
+
 }
