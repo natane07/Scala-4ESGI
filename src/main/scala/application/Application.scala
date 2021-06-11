0e820effa6d0a3ae2892846ad9c9a5579b15ee19
Natane <natane-b@hotmail.fr>
2021-06-11 19:57:39 +0200

    Insertion des vidéos tendance en BDD

--- src/main/scala/application/Application.scala
+++ src/main/scala/application/Application.scala
@@ -6,8 +6,11 @@ object Application {
 
   def main(args: Array[String]): Unit = {
     // Recuperer les informations d'une chaine, des playlist et des videos (limite de 5)
-    val idChannel = Channel.searchChannel("SEB")
+    val idChannel = Channel.searchChannel("Skyrroz")
     val channel = Channel.getChannel(idChannel)
+
+    // Api pour recuperer les 10 premieres videos en tendance et les inserer en BDD
+    Video.getVideoTendance()
   }
 
 }
